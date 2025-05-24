00da2c4a802caa51229b74fcbe1c3ea5e66d2d35
kuro-neko-xyz <a.aleman.h@icloud.com>
2025-05-24 16:57:51 -0600

    JAM-01: Tab Between Options

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -4,13 +4,13 @@ import styles from "./styles.module.scss";
 
 const Option: FC<Track> = ({ name, artist, album, cover }) => {
   return (
-    <div className={styles.option}>
+    <button className={styles.option} type="button">
       <img src={cover} alt={`${album} album cover`} />
       <h3>{name}</h3>
       <p>
         {artist} - {album}
       </p>
-    </div>
+    </button>
   );
 };
 
