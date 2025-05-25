8e5c4d504382518b4f7f5b0f23709d1d2821c164
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-05-25 17:23:36 -0600

    JAM-16: Add Submit Button

--- src/components/Playlist/index.tsx
+++ src/components/Playlist/index.tsx
@@ -9,16 +9,25 @@ interface PlaylistProps {
 }
 
 const Playlist: FC<PlaylistProps> = ({ onTrackClick, playlist }) => {
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <div className={styles.container}>
       <div className={playlist.length ? styles.playlist : styles.emptyPlaylist}>
-        {playlist.map((track) => (
-          <TrackInPlaylist
-            key={`${track.name}-${track.artist}`}
-            onTrackClick={onTrackClick}
-            track={track}
-          />
-        ))}
+        <div className={styles.songsContainer}>
+          {playlist.map((track) => (
+            <TrackInPlaylist
+              key={`${track.name}-${track.artist}`}
+              onTrackClick={onTrackClick}
+              track={track}
+            />
+          ))}
+        </div>
+        <form className={styles.form} onSubmit={handleFormSubmit}>
+          <input type="submit" value="Save playlist" />
+        </form>
       </div>
     </div>
   );
